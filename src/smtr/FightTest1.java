8c737225663c15cc479f61d8a4d8deb8a7477c02
balaji raju <balaji2raju@gmail.com>
2017-03-21 12:11:50 +0530

    Adr 1st attempt

---
+++ src/smtr/FightTest1.java
@@ -0,0 +1,10 @@
+package smtr;
+
+public class FightTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" I am in Fight test 1222 class");
+	}
+
+}
